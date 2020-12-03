f90fffade51d3437d59532c95e4005ed791df8b2
malkeeszon <rwitczak89@gmail.com>
2020-12-03 20:40:27 +0100

    feat(select-destination.ts): correctly update state and retrieve data

--- client/src/features/SelectDestination.tsx
+++ client/src/features/SelectDestination.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useState, useEffect, useRef } from 'react';
-import { connect, useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { RootState } from '../store';
+import { setCurrentDestination } from '../store/actions';
 import { AppState, MapActionTypes } from '../store/types';
 
 
@@ -10,54 +11,148 @@ interface ISelectDestinationProps {}
 
 export const SelectDestination: React.FC<ISelectDestinationProps> = () => {
 
-  const [isClicked, setIsClicked] = useState(false);
+  const [isFromClicked, setIsFromClicked] = useState(false);
+  const [isOKClicked, setIsOKClicked] = useState(false);
+  const [isSubmited, setIsSybmited] = useState(false);
+  const [newDestination, setNewDestination] = useState('');
 
   const history = useHistory();
   const user = useSelector((state: RootState) => state.user);
+  const destination = useSelector((state: RootState) => state.destination);
+  const dispatch = useDispatch();
+  const inputFields = document.getElementsByTagName('input');
 
   useEffect(() => {
     console.log('AFTER LOGIN', user.favourites);
   }, []);
 
 
-  function handleCLick(): void {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleSubmit(event: any): void {
+    event.preventDefault();
     console.log('to map');
+    dispatch(setCurrentDestination({
+      destination: newDestination,
+      latitude: user.latitude,
+      longitude: user.longitude
+    }));
     history.push('/map');
+  }
+
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleOKClick(): void {
+    dispatch(setCurrentDestination({
+      destination: newDestination,
+      latitude: user.latitude,
+      longitude: user.longitude
+    }));
+    setIsOKClicked(!isOKClicked);
+    inputFields[0].value = '';
+    inputFields[0].disabled = true;
+
+  }
+
+  function handleChangeClick(): void {
+    inputFields[0].value = '';
+    setNewDestination('');
+    dispatch(setCurrentDestination({
+      destination: '',
+      latitude: 0,
+      longitude: 0
+    }));
+    setIsOKClicked(!isOKClicked);
+    inputFields[0].disabled = false;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleChange(event: any): void {
+    setNewDestination(event.target.value);
   }
 
   return (
     <div>
-      <h1>{user.name}</h1>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+      <h1>{user.username}</h1>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '100%'
+        }}
+      >
         <h1>
           Chose destination
         </h1>
-        <div style={{ flexShrink: 0, width: '100%' }}>
-          <form>
-            <div onClickCapture={() => setIsClicked(true)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+        <div
+          style={{
+            flexShrink: 0,
+            width: '100%'
+          }}
+        >
+          <form onSubmit={handleSubmit}>
+            <div
+              onClickCapture={() => setIsFromClicked(true)}
+              onChange={handleChange}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '100%'
+              }}
+            >
               <input
                 type="text"
-                // ref={inputReference}
                 placeholder='Example: carrer Sant Miquel 7, Barcelona'
-                style={{ width: '80%', height: '30px' }}
+                style={{
+                  width: '80%',
+                  height: '30px'
+                }}
               />
-              {isClicked ? (
+              <button
+                disabled={isOKClicked}
+                id='OK_button'
+                type='button'
+                onClick={handleOKClick}
+              > OK
+              </button>
+              {isFromClicked ? (
                 <div>
                   <h2>Favoirites</h2>
                   {user.favourites && user.favourites.map((favourite) => (
                     <div key={favourite.label}>
-                      <p> Name: {favourite.label} </p>
+                      <h3>{favourite.label}</h3>
                       <p>Address: {favourite.destination}</p>
                     </div>
                   ))}
                 </div>) : null}
-              <button
-                type='submit'
-                onClick={handleCLick}
-              >Take me there
-              </button>
             </div>
           </form>
+        </div>
+        <div />
+        <div>
+          {isOKClicked ? (
+            <div>
+              <h2>You are heading to:</h2>
+              <div key={destination && destination.destination}>
+                <h3>{destination && destination.destination}</h3>
+              </div>
+              <button
+                type='button'
+                onClick={handleChangeClick}
+              > CHANGE
+              </button>
+            </div>) : null}
+        </div>
+        <div
+          style={{
+            paddingTop: '40px'
+          }}
+        >
+          <button
+            type='submit'
+          >Take me there
+          </button>
         </div>
       </div>
     </div>
