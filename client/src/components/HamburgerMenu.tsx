4563cc96ada75cc9cd39cb9c5a296cafe81c45b6
Carlos <carlosdsv@gmail.com>
2020-12-04 11:01:17 +0100

    feat(hamburguer menu): add routes for the menu
    routes to favorite, providers and logout

--- client/src/components/HamburgerMenu.tsx
+++ client/src/components/HamburgerMenu.tsx
@@ -1,9 +1,50 @@
+import { Button, Menu, MenuItem } from '@material-ui/core';
 import * as React from 'react';
+import { MouseEvent, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
 
 interface IHamburgerMenuProps {}
 
 const HamburgerMenu: React.FC<IHamburgerMenuProps> = () => {
-  return <div>HamburgerMenu</div>;
+  const history = useHistory();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleDestination = () => {
+    history.push('/favourites');
+  };
+  const handleLogout = () => {
+    history.push('/');
+  };
+  return (
+    <>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MenuIcon fontSize="large" />
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleDestination}>Favorites</MenuItem>
+        <MenuItem onClick={handleClose}>Providers</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+    </>
+  );
 };
 
 export default HamburgerMenu;
