b10bc31f7077fa1ec0d702fe4ae35de9f18451d0
Carlos <carlosdsv@gmail.com>
2020-12-04 11:04:45 +0100

    feat(moto info): add style for MotoInfo component

---
+++ client/src/components/MapInfoStyle.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const MotoInfoContainerDiv = styled.div`
+  height: 200px;
+  width: 414px;
+  box-sizing: border-box;
+  background-color: #FFF;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  border-radius: 15px 15px 0px 0px;
+  box-shadow: 0px 0px 8px 8px rgba(0,0,0,0.11);
+  z-index-2;
+  margin: 0px;
+  padding: 0px;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+export const MotoInfoTextDiv = styled.div`
+  height: 150px;
+  width: 414px;
+  margin: 0px;
+  padding: 0px;
+`;
+
+export const MotoInfoButtonDiv = styled.div`
+  height: 50px;
+  width: 414px;
+  margin-botton: 10px;
+  margin-left: 250px;
+`;
