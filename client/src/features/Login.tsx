c46fa25daa12cdbf59100be4a3776eeb01f6a4c6
malkeeszon <rwitczak89@gmail.com>
2020-12-06 18:06:03 +0100

    feat(login): add background video

--- client/src/features/Login.tsx
+++ client/src/features/Login.tsx
@@ -1,26 +1,52 @@
 /* eslint-disable no-console */
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
+import clsx from 'clsx';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+
 import { RootState } from '../store';
 import { authenticateUser, getUserData, storeUserLocation } from '../store/actions';
 import { AppState, STORE_USER_DATA, User } from '../store/types';
-import { Video } from './LoginStyle';
-import { LoginDiv } from './SelectDestinationStyle';
-import video from '../../video/SampleVideo_1280x720_1mb.mp4';
+import { Video, LoginDiv, WrapperDiv, FormDiv } from './LoginStyle';
+import video from '../video/background-login-video.mp4';
+
 
 
 interface ILoginProps {}
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      margin: theme.spacing(1),
+    },
+    textField: {
+      width: '25ch',
+    },
+
+    color: {
+      primary: '#ffff'
+    }
+  })
+);
+
 export const Login: React.FC<ILoginProps> = () => {
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState({ password: '', showPassword: false });
   const [locationPermission, setLocationPermission] = useState(false);
   const [userLocation, setUserLocation] = useState({
-    latitude: 0,
-    longitude: 0
+    latitude: 41.385871,
+    longitude: 2.167633
   });
+
+  const classes = useStyles();
 
   const dispatch = useDispatch();
   const history = useHistory();
@@ -45,10 +71,10 @@ export const Login: React.FC<ILoginProps> = () => {
       console.log('LAT: ', location.coords.latitude);
       console.log('LONG: ', location.coords.longitude);
 
-      setUserLocation({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude
-      });
+      // setUserLocation({
+      //   latitude: location.coords.latitude,
+      //   longitude: location.coords.longitude
+      // });
     });
   }
 
@@ -67,10 +93,13 @@ export const Login: React.FC<ILoginProps> = () => {
     setUsername(event.target.value);
   }
 
-  function handlePasswordChange(
-    event: { target: { value: React.SetStateAction<string>; };
-    }) {
-    setPassword(event.target.value);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handlePasswordChange(event: any) {
+    setPassword({ ...password, password: event.target.value });
+  }
+
+  function handleClickShowPassword() {
+    setPassword({ ...password, showPassword: !password.showPassword });
   }
 
   function handleLocationPermissionChange(
@@ -79,42 +108,71 @@ export const Login: React.FC<ILoginProps> = () => {
     setLocationPermission(event.target.checked);
   }
 
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
   return (
-    <Video loop autoPlay muted>
-      <source src={video} />
-      <form onSubmit={handleSubmit}>
+    <>
+      <WrapperDiv>
+        <Video loop autoPlay muted>
+          <source src={video} />
+        </Video>
         <LoginDiv>
-          <h1>
-            LOGIN
-          </h1>
-          <input
-            onChange={handleUsernameChange}
-            type='text'
-            name='username'
-            placeholder='login'
-          />
-          <input
-            onChange={handlePasswordChange}
-            type='password'
-            name='password'
-            placeholder='password'
-          />
-          <label
-            htmlFor='geo-location'
-          > Allow location services
-            <input
-              onChange={handleLocationPermissionChange}
-              type='checkbox'
-              name='geo-location'
-              required={true}
-            />
-          </label>
-          <Button type='submit' variant="contained" color="primary">
+          <FormDiv onSubmit={handleSubmit}>
+            <FormControl
+              className={clsx(classes.margin, classes.textField, classes.color)}
+            >
+              <InputLabel htmlFor="standard-adornment-login">Username</InputLabel>
+              <Input
+                id="standard-size-normal"
+                color='primary'
+                type='text'
+                onChange={handleUsernameChange}
+              />
+            </FormControl>
+            <FormControl
+              className={clsx(classes.margin, classes.textField, classes.color)}
+            >
+              <InputLabel htmlFor="standard-adornment-password" color='primary'>Password</InputLabel>
+              <Input
+                id="standard-size-normal"
+                type={password.showPassword ? 'text' : 'password'}
+                value={password.password}
+                onChange={handlePasswordChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {password.showPassword ?
+                        <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+            }
+              />
+            </FormControl>
+            <label
+              htmlFor='geo-location'
+            > Allow location services
+              <input
+                onChange={handleLocationPermissionChange}
+                type='checkbox'
+                name='geo-location'
+                required={true}
+              />
+            </label>
+          </FormDiv>
+          <Button type='submit' variant='contained' color='default' onClick={handleSubmit}>
             LOGIN
           </Button>
         </LoginDiv>
-      </form>
-    </Video>
+      </WrapperDiv>
+    </>
   );
 };
 
--- client/src/features/LoginStyle.ts
+++ client/src/features/LoginStyle.ts
@@ -1,13 +1,42 @@
 import styled from 'styled-components';
 
 export const Video = styled.video`
+  position: fixed;
+  z-index: -1;
 
-position: absolute;
-width: 100%;
-left: 50%;
-top: 50%;
-height: 100%;
-object-fit: 'cover';
-transform: 'translate(-50%, -50%);
-zIndex: 1
+  @media (min-aspect-ratio: 16/9) {
+    width: auto;
+  }
+  @media (max-aspect-ratio: 16/9) {
+    height: 800px;
+  }
+
+  background-color: black;
+`;
+
+export const LoginDiv = styled.div`
+  position: fixed;
+  top: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: auto;
+  z-index: 1;
+`;
+
+export const FormDiv = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 2;
+`;
+
+export const WrapperDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: auto;
+  width: auto;
+  color: white;
 `;
