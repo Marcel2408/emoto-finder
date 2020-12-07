2dcbccd0cdbb8b826d7ecfe8dd7b07e46f1e2141
malkeeszon <rwitczak89@gmail.com>
2020-12-07 17:08:58 +0100

    feat(login): improve login page

--- client/src/features/Login.tsx
+++ client/src/features/Login.tsx
@@ -1,38 +1,56 @@
 /* eslint-disable no-console */
-import { Button, IconButton } from '@material-ui/core';
+import { Button, IconButton, InputLabel } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
+import { createStyles, Theme, makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
-import { Visibility, VisibilityOff } from '@material-ui/icons';
+import { BorderBottom, Visibility, VisibilityOff } from '@material-ui/icons';
+import purple from '@material-ui/core/colors/purple';
+import grey from '@material-ui/core/colors/grey';
+import common from '@material-ui/core/colors/common';
 
 import { RootState } from '../store';
 import { authenticateUser, getUserData, storeUserLocation } from '../store/actions';
 import { AppState, STORE_USER_DATA, User } from '../store/types';
 import { Video, LoginDiv, WrapperDiv, FormDiv } from './LoginStyle';
-import video from '../video/background-login-video.mp4';
+import video from '../assets/video/background-login-video.mp4';
 
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: common.white }
+  }
+
+});
 
 interface ILoginProps {}
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((theme1: Theme) =>
   createStyles({
     margin: {
-      margin: theme.spacing(1),
+      margin: theme1.spacing(1),
     },
     textField: {
       width: '25ch',
+      height: '3em',
+      textColor: 'primary'
     },
 
-    color: {
-      primary: '#ffff'
+    root: {
+      '& .MuiInputLabel-formControl': {
+        position: 'static'
+      }
+    },
+    input: {
+      color: 'white',
+      fontWeight: 600,
+      fontSize: 20
     }
   })
 );
@@ -40,10 +58,14 @@ const useStyles = makeStyles((theme: Theme) =>
 export const Login: React.FC<ILoginProps> = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState({ password: '', showPassword: false });
-  const [locationPermission, setLocationPermission] = useState(false);
+  const [locationPermission, setLocationPermission] = useState(true);
+  const [fieldFocus, setFieldFocus] = useState({
+    username: false,
+    password: false
+  });
   const [userLocation, setUserLocation] = useState({
-    latitude: 41.385871,
-    longitude: 2.167633
+    latitude: 0,
+    longitude: 0
   });
 
   const classes = useStyles();
@@ -71,10 +93,10 @@ export const Login: React.FC<ILoginProps> = () => {
       console.log('LAT: ', location.coords.latitude);
       console.log('LONG: ', location.coords.longitude);
 
-      // setUserLocation({
-      //   latitude: location.coords.latitude,
-      //   longitude: location.coords.longitude
-      // });
+      setUserLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      });
     });
   }
 
@@ -98,21 +120,27 @@ export const Login: React.FC<ILoginProps> = () => {
     setPassword({ ...password, password: event.target.value });
   }
 
-  function handleClickShowPassword() {
-    setPassword({ ...password, showPassword: !password.showPassword });
-  }
-
   function handleLocationPermissionChange(
     event: { target: { checked: React.SetStateAction<boolean>; };
     }) {
     setLocationPermission(event.target.checked);
   }
 
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
+  function handleOnUsernameFieldFocus() {
+    setFieldFocus({ ...fieldFocus, username: true });
+  }
+
+  function handleOnUsernameFieldBlur() {
+    setFieldFocus({ ...fieldFocus, username: false });
+  }
+  function handleOnPasswordFieldFocus() {
+    setFieldFocus({ ...fieldFocus, password: true });
+  }
+
+  function handleOnPasswordFieldBlur() {
+    setFieldFocus({ ...fieldFocus, password: false });
+  }
+
 
   return (
     <>
@@ -120,57 +148,62 @@ export const Login: React.FC<ILoginProps> = () => {
         <Video loop autoPlay muted>
           <source src={video} />
         </Video>
-        <LoginDiv>
-          <FormDiv onSubmit={handleSubmit}>
-            <FormControl
-              className={clsx(classes.margin, classes.textField, classes.color)}
-            >
-              <InputLabel htmlFor="standard-adornment-login">Username</InputLabel>
-              <Input
-                id="standard-size-normal"
-                color='primary'
-                type='text'
-                onChange={handleUsernameChange}
-              />
-            </FormControl>
-            <FormControl
-              className={clsx(classes.margin, classes.textField, classes.color)}
-            >
-              <InputLabel htmlFor="standard-adornment-password" color='primary'>Password</InputLabel>
-              <Input
-                id="standard-size-normal"
-                type={password.showPassword ? 'text' : 'password'}
-                value={password.password}
-                onChange={handlePasswordChange}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                    >
-                      {password.showPassword ?
-                        <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-            }
-              />
-            </FormControl>
-            <label
-              htmlFor='geo-location'
-            > Allow location services
-              <input
-                onChange={handleLocationPermissionChange}
-                type='checkbox'
-                name='geo-location'
-                required={true}
-              />
-            </label>
-          </FormDiv>
-          <Button type='submit' variant='contained' color='default' onClick={handleSubmit}>
-            LOGIN
-          </Button>
-        </LoginDiv>
+        <ThemeProvider theme={theme}>
+          <LoginDiv>
+            <FormDiv onSubmit={handleSubmit}>
+              <FormControl
+                className={clsx(
+                  classes.margin, classes.textField, classes.root
+                )}
+              >
+                <TextField
+                  inputProps={{ className: classes.input }}
+                  className={fieldFocus.username ? 'textfield_focus' : null}
+                  InputLabelProps={{ className: 'textfield_not_focus' }}
+                  label='Username'
+                  id='standard-basic'
+                  type='text'
+                  onFocus={handleOnUsernameFieldFocus}
+                  onBlur={handleOnUsernameFieldBlur}
+                  onChange={handleUsernameChange}
+                />
+              </FormControl>
+              <FormControl
+                className={clsx(
+                  classes.margin, classes.textField, classes.root
+                )}
+              >
+                <TextField
+                  inputProps={{ className: classes.input }}
+                  className={fieldFocus.password ? 'textfield_focus' : null}
+                  label='Password'
+                  InputLabelProps={{ className: 'textfield_not_focus' }}
+                  id='standard-basic'
+                  type='password'
+                  value={password.password}
+                  onFocus={handleOnPasswordFieldFocus}
+                  onBlur={handleOnPasswordFieldBlur}
+                  onChange={handlePasswordChange}
+                />
+              </FormControl>
+
+              <label
+                htmlFor='geo-location'
+              > Allow location services
+                <input
+                  onChange={handleLocationPermissionChange}
+                  type='checkbox'
+                  name='geo-location'
+                  required={true}
+                  checked
+                />
+              </label>
+              <Button type='submit' variant='contained' color='default' onClick={handleSubmit}>
+                LOGIN
+              </Button>
+            </FormDiv>
+          </LoginDiv>
+        </ThemeProvider>
       </WrapperDiv>
     </>
   );
--- client/src/features/LoginStyle.ts
+++ client/src/features/LoginStyle.ts
@@ -5,10 +5,10 @@ export const Video = styled.video`
   z-index: -1;
 
   @media (min-aspect-ratio: 16/9) {
-    width: auto;
+    width: 100wh;
   }
   @media (max-aspect-ratio: 16/9) {
-    height: 800px;
+    height: 100hv;
   }
 
   background-color: black;
@@ -16,13 +16,14 @@ export const Video = styled.video`
 
 export const LoginDiv = styled.div`
   position: fixed;
-  top: 400px;
+  top: 430px;
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
   height: auto;
   z-index: 1;
+  margin-bottom: 5em;
 `;
 
 export const FormDiv = styled.form`
@@ -30,6 +31,19 @@ export const FormDiv = styled.form`
   flex-direction: column;
   align-items: center;
   z-index: 2;
+  font-size: 1.2em;
+  .textfield_not_focus {
+    color: white !important;
+    font-weight: 600;
+  }
+  .textfield_focus {
+    color: white !important;
+    background-color: grey !important;
+    opacity: 0.8 !important;
+    padding: 0.25em !important;
+    font-size: xx-medium !important;
+    font-weight: 600;
+  }
 `;
 
 export const WrapperDiv = styled.div`
