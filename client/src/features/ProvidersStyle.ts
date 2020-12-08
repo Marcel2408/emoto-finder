cd7db74052d602d60b6f2f5faa503479967afab1
Ewa Rasala <ewakrasala@gmail.com>
2020-12-08 20:35:03 +0100

    add providers-component styling

--- client/src/features/ProvidersStyle.ts
+++ client/src/features/ProvidersStyle.ts
@@ -1,3 +1,32 @@
 import styled from 'styled-components';
 
-export const Div = styled.div``;
+export const FlexWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow-y: hidden;
+`;
+
+export const ProviderHeader = styled.h1`
+  width: 100%;
+  text-align: center;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0;
+  padding: 0.75em 0.15em;
+  font-size: 1.5em;
+  background-color: #303f9f;
+  color: white;
+  position: relative;
+`;
+
+export const ProvidersContainerDiv = styled.div`
+  flex: 1;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 1em;
+  font-size: 150%;
+`;
